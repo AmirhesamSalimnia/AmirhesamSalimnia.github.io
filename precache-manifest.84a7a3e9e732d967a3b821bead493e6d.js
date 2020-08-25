2f265915d5a6c9813894bbc30b1daedbcf75dd1b
AmirhesamSalimnia <hesam.77s@gmail.com>
2020-08-26 01:26:52 +0430

    Updates

---
+++ precache-manifest.84a7a3e9e732d967a3b821bead493e6d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a3365f157951737c9964bb03fe1ab81",
+    "url": "/index.html"
+  },
+  {
+    "revision": "18ee693fe33acf9aeb4f",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "377f33bb7682d0c2f129",
+    "url": "/static/css/main.9fc689ea.chunk.css"
+  },
+  {
+    "revision": "18ee693fe33acf9aeb4f",
+    "url": "/static/js/2.11047eb6.chunk.js"
+  },
+  {
+    "revision": "80fa05aac9f80c71b5f99ecec6ead10d",
+    "url": "/static/js/2.11047eb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "377f33bb7682d0c2f129",
+    "url": "/static/js/main.3978b8d4.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "48d5aae9c6953b870e8446cb7184fdfd",
+    "url": "/static/media/resume.48d5aae9.pdf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/home/precache-manifest.9df64401db61b61d40932fac39e777e3.js"
+  "/precache-manifest.84a7a3e9e732d967a3b821bead493e6d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/home/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
